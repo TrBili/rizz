4d363136940b7d05b68f8e73596793a9307526cc
TrBili <mosbill067@gmail.com>
2024-01-17 20:39:40 -0800

    hah

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,19 @@
 "use client"
-
+import { useState } from 'react'
+import Form from '@/components/Form'
+import Result from '@/components/Result.jsx'
 import { calculate } from "@/utility/helpers" // I added this function beforehand
 
+// the @ reference to the root level
+
 export default function Home() {
+  const [score, setScore]= useState(0);
+  
+  function calculateScore(name, major){
+    setScore(calculate(name, major));
+  }
+  
+  
   return (
     <main style={{
       padding: 100,
@@ -17,8 +28,13 @@ export default function Home() {
         style={{ width: 100 }} />
       <h1>Hello 👋</h1>
       <p>
-        We're so glad you're here :)
+        We're so glad you're here 
       </p>
+      {score== 0 ?
+      <Form handleCalculate={calculateScore}/>
+      : <Result score={score}/>
+      }
+      {/* you can plug in any js elemnets as parameters */}
     </main>
   )
 }
---
+++ components/Result.jsx
@@ -0,0 +1,17 @@
+export default function Result(props){
+    return(
+        <div>
+            Your score: {props.score}
+        </div>
+    )
+}
+
+// you have props which allows you to extend 
+// parameters
+
+
+// hooks:
+// React features: state, component life cycle, etc
+
+// useStates
+// const [state name, state updateing function name]= useState(initial value)
